da78ce230152c9c82520e7cc98b1d9be8e3516cc
verflou <112292683+Elinn-16@users.noreply.github.com>
2023-12-04 15:18:14 -0300

    node

--- app.js
+++ app.js
@@ -3,16 +3,15 @@ const path = require ('path');
 const mongoose = require('mongoose');
 
 const app = express();
-const uri = "const uri = "mongodb+srv://notas:hector@cluster0.onoq98e.mongodb.net/?retryWrites=true&w=majority";";
+const uri = "mongodb+srv://notas:hector@cluster0.onoq98e.mongodb.net/?retryWrites=true&w=majority";
 const PORT = 3000; 
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 
-mongoose.connect(uri,
-    {useNewUrlParser:true,useUnifiedTopology:true})
-    
+mongoose
+    .connect(uri,)
     .then(console.log("Connected to database"))
     .catch((error)=>console.log("tienes un error, tu error es :" + error))
 
